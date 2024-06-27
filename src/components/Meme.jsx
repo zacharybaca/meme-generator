3a1aa599f54d0d7c2ade3ec6a14ae42259073c79
Zachary Baca <zachary.baca@msn.com>
2024-06-27 12:45:09 -0500

    Update Meme.jsx

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -7,12 +7,12 @@ export default function Meme() {
                 <div id="form-container">
                     <div id="top-text-container">
                         <label for="top-text">Top Text</label>
-                        <input id="top-text" name="top-text"/>
+                        <input id="top-text" name="top-text" placeholder="Enter Text For Top of Meme"/>
                     </div>
                     
                     <div id="bottom-text-container">
                         <label for="bottom-text">Bottom Text</label>
-                        <input id="bottom-text" name="bottom-text"/>
+                        <input id="bottom-text" name="bottom-text" placeholder="Enter Text For Bottom of Meme"/>
                     </div>
                     
                 </div>
