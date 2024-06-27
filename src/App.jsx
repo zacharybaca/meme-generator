c290a58f1bc2b04d28742cfa2ac8ffb52cfe40c2
Zachary Baca <zachary.baca@msn.com>
2024-06-27 12:14:54 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Header from "./components/Header";
+import Meme from "./components/Meme";
 
 function App() {
  
 
   return (
-    <div>
+    <div id="app-container">
       <Header />
+      <Meme />
     </div>
   )
 }
