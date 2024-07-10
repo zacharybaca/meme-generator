df82963cff984bb62f6d549da3029d8189206dae
Zachary Baca <zachary.baca@msn.com>
2024-07-10 12:00:57 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,29 +8,57 @@ function App() {
   const [meme, setMeme] = React.useState({
     topText: "",
     bottomText: "",
-    url: ""
+    url: "http://i.imgflip.com/1bij.jpg" 
   });
 
   // Set-up State for Getting all Memes from API
-  const [allMemes, setAllMemes] = React.useState([]);
+  const [allMemes, setAllMemes] = React.useState();
+
+  // Get Memes From API and Save to allMemes
+  React.useEffect(() => {
+    async function getMemes ()  {
+      const response = await fetch("https://api.imgflip.com/get_memes")
+      const data = await response.json()
+      setAllMemes(data.data.memes)
+    }
+
+    getMemes();
+  }, [])
 
   // Function That Will Fire When Get Meme Button is Clicked
-  function getMemeImage(e) {
-    // Prevents Form From Refreshing After Submit
-    e.preventDefault();
+  function getMemeImage() {
+    
     // Create Random Number That is Less Than Length of memesArray
     // To Pick Random URL
     const randomNumber = Math.floor(Math.random() * allMemes.length);
 
+    // Get Random Meme Image
+    const memeImg = allMemes[randomNumber].url;
+
     // Set Meme Image to Random URL
-    setMeme(allMemes[randomNumber].url);
+    setMeme(prevState => ({
+      ...prevState,
+      url: memeImg
+    }));
     console.log('Random URL: ', meme)
+}
+
+// Function to Incorporate Input State Within React
+function handleChange(event) {
+  const {name, value} = event.target;
+
+  event.preventDefault();
+
+  setMeme(prevState => ({
+    ...prevState,
+    [name]: value
+  }))
 }
 
   return (
     <div id="app-container">
       <Header />
-      <Meme getMeme={getMemeImage} meme={meme}/>
+      <Meme getMeme={getMemeImage} meme={meme} handleChange={handleChange}/>
     </div>
   )
 }
